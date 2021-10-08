bd7da8b50e82ba8c8d9aff957e523899cf0648a0
Wind/owZ <windowz414@gnuweeb.org>
2021-10-08 04:18:05 +0300

    build: Refactor to Kasumi
    * This will not be the same as Stellar-Weeb. Now we have a
      different type of product.
    
    Signed-off-by: Wind/owZ <windowz414@gnuweeb.org>

--- core/config.mk
+++ core/config.mk
@@ -296,7 +296,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(LINEAGE_BUILD),)
-include vendor/materium/config/BoardConfigLineage.mk
+include vendor/kasumi/config/BoardConfigLineage.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
